fca9aee46f59174c0947222aa5546c298036d726
Carlos Francisco Nuñez Delgado <cnunez1@ucol.mx>
2020-11-01 11:56:51 -0600

    Agregar archivos de proyecto.

---
+++ Aplicacion/Cursos/Eliminar.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Aplicacion.ManejadorError;
+using MediatR;
+using Persistencia;
+
+namespace Aplicacion.Cursos
+{
+    public class Eliminar
+    {
+        public class Ejecuta : IRequest {
+            public int Id{get;set;}
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext _context;
+            public Manejador(CursosOnlineContext context){
+                _context = context;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var curso = await _context.Curso.FindAsync(request.Id);
+                if(curso == null)
+                {
+                    //throw new Exception("No se puede eliminar");
+                    throw new ManejadorExcepcion(HttpStatusCode.NotFound, new { curso = "No se encontro el curso" });
+                }
+
+
+                _context.Remove(curso);
+
+                var resultado = await _context.SaveChangesAsync();
+
+                if(resultado > 0){
+                    return Unit.Value;
+                }
+
+                throw new Exception("Bi se pudieron guardar los cambios");
+                    
+
+            }
+        }
+
+
+    }
+}
